ee217e7b634039fe88e09c0de2777f88a04edb84
RadoslawWodnicki <radwod002@utp.edu.pl>
2021-03-22 19:41:39 +0100

    first commit

---
+++ src/main/java/pl/edu/utp/meancalculation/GeometricMeanAlgorithm.java
@@ -0,0 +1,25 @@
+package pl.edu.utp.meancalculation;
+
+public class GeometricMeanAlgorithm implements MeanAlgorithm {
+
+	// fields - pola, wewnetrzny stan naszego algorytmu. nikt nie powinien ich edytowac
+    private double currentSum;
+    private double noOfFactors;
+
+    // constructor
+    public GeometricMeanAlgorithm() {
+        currentSum = 1;
+        noOfFactors = 0;
+    }
+
+    public void add(double factor) {
+        currentSum *= factor;
+        noOfFactors++;
+    }
+
+    @Override
+    public double calculateMean() {
+        return Math.pow(currentSum, (double) 1 / noOfFactors);
+    }
+
+}
---
+++ src/main/java/pl/edu/utp/meancalculation/MeanAlgorithm.java
@@ -0,0 +1,9 @@
+package pl.edu.utp.meancalculation;
+
+public interface MeanAlgorithm {
+
+    void add(double factor);
+
+    double calculateMean();
+
+}
